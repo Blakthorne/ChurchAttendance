b86a33218a5b479a0dd5bd0035b268bbc9244cdf
David Polar <dlpolar38@gmail.com>
2019-04-04 19:11:46 -0400

    added initial files

---
+++ attendance.py
@@ -0,0 +1,87 @@
+import sys
+import datetime
+
+class Person:
+    def __init__(self, lastName, firstName, status, phoneNum):
+        self.lastName = lastName
+        self.firstName = firstName
+        self.status = status
+        self.phoneNum = phoneNum
+        # self.numWeeksPresent = numWeeksPresent
+        # self.numWeeksAbsent = numWeeksAbsent 
+
+# create lists for info
+allPeople = list()
+
+def writeToList(Person):
+    file = open("list.txt", "a+")
+    info = Person.lastName + "," + Person.firstName + "," + Person.status + "," + Person.phoneNum + "\n"
+    file.write(info)
+    file.close()
+
+def readFromList():
+    with open("list.txt") as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        info = line.split(",")
+        newPerson = Person(info[0], info[1], info[2], info[3])
+        allPeople.append(newPerson)
+
+def getDate():
+    while True:
+        isValidDate = True
+        date = input("Date? (mm/dd/yyyy)  ")
+        if (len(date) == 10) & (date[2:3] == "/") & (date[5:6] == "/"):
+            dateParts = date.split("/")
+            try:
+                dateFormatted = datetime.date(int(dateParts[2]), int(dateParts[0]), int(dateParts[1]))
+            except:
+                isValidDate = False
+            if (isValidDate):
+                break
+        print("Sorry, but that was not a valid option. Please enter again.\n")
+    return dateFormatted
+
+def getService():
+    while True:
+        service = input("Morning or Evening?  ").lower()
+        if ((service == "morning") | (service == "evening")):
+            break
+        print("Sorry, but that was not a valid option. Please enter again.\n")
+    return service
+
+def askToAdd():
+    while True:
+        while True:
+            plusPerson = input("Do you want to add another person?  ").lower()
+            if ((plusPerson == "yes") | (plusPerson == "no")):
+                break
+            print("Sorry, but that was not a valid option. Please enter again.\n")
+        if (plusPerson == "no"):
+            break
+        addPerson()
+
+def addPerson():
+    firstName = input("What is their first name?  ").lower()
+    lastName = input("What is their last name?  ").lower()
+    while True:
+        status = input("What is their status? (\"Member\", \"First Time-Visitor\", or \"Returning Visitor\")  ").lower()
+        if ((status == "member") | (status == "first time visitor") | (status == "returning visitor")):
+            break
+        print("Sorry, but that was not a valid option. Please enter again.\n")
+    while True:
+        phoneNum = input("What is their phone number (\"(xxx) xxx-xxxx\")  ")
+        if ((phoneNum[0:1] == "(") & (phoneNum[4:5] == ")") & (phoneNum[5:6] == " ") & (phoneNum[9:10] == "-")):
+            break
+        print("Sorry, but that was not a valid option. Please enter again.\n")
+    print("")
+    print("")
+    newPerson = Person(lastName, firstName, status, phoneNum)
+    allPeople.append(newPerson)
+
+date = getDate()
+# service = getService()
+askToAdd()
+for person in allPeople:
+    writeToList(person)
+readFromList() 
---
+++ person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, firstName, lastName, status, phoneNum):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.status = status
+        self.phoneNum = phoneNum
+        # self.numWeeksPresent = numWeeksPresent
+        # self.numWeeksAbsent = numWeeksAbsent  
